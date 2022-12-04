3ed269ebde388bb1f8cce2dbce1749203075feb1
idiyrom <roman.idiyatullin@gmail.com>
2022-12-04 12:32:04 -0500

    Leetcode - 14. Longest Common Prefix  Optimal solution - beats 94% runtime, 48.6% memory

--- src/easy/LongestPrefix.java
+++ src/easy/LongestPrefix.java
@@ -33,29 +33,3 @@ public class LongestPrefix {
         System.out.println("Execution took: " + (endTime-startTime));
     }
 }
-//166211
-
-//159979
-
-/*
-
-        public String longestCommonPrefix(String[] strs) {
-
-        if(strs == null || strs.length == 0) return "";
-        if(strs.length==1) return strs[0];
-
-        for (int i = 0; i < strs[0].length(); i++)
-        {
-            char letter = strs[0].charAt(i);
-
-            for (int j = 1; j < strs.length; j++) {
-                if (i == strs[j].length() || letter != strs[j].charAt(i)) {
-                    return strs[0].substring(0,i);
-                }
-            }
-        }
-        return strs[0];
-    }
-}
-
- */
