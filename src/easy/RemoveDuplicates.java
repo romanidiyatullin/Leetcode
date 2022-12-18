1bf5acaca62b2454dca0016c9884354f33120cd5
idiyrom <roman.idiyatullin@gmail.com>
2022-12-18 13:55:56 -0500

    Leetcode - 26. Solution Attempt 3: After using ALL tips and reviewing solutions from others. FINALLY using statements about "non-decreasing" & "sorted" facts.. Runtime - beats 100% (within 1ms) Memory - beats 21.3% (within majority) Assume as GOLDEN SOLUTION (Etalon)

--- src/easy/RemoveDuplicates.java
+++ src/easy/RemoveDuplicates.java
@@ -1,43 +1,20 @@
 package easy;
 
-import java.util.Arrays;
-
 public class RemoveDuplicates {
     public int removeDuplicates(int[] nums) {
-
-        if(nums == null || nums.length == 0 || nums.length > 30000)
-            return -1;
-
-        int duplicates = 0;
-        int l = nums.length-1;
-
-        for(int i=0; i<=l; i++)
-        {
-
-            if (nums[i] == 999)
-            {
-                nums[i] = nums[l];
-                nums[l] = 999;
-                l--;
+        int indexToInsert = 1;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i-1] != nums[i]){
+                nums[indexToInsert] = nums[i];
+                indexToInsert++;
             }
-
-            for (int j = i + 1; j <= l; j++)
-            {
-                if (nums[i] == nums[j]) {
-                    nums[j] = 999;
-                    duplicates++;
-                }
-            }
-
-            while (nums[l] == 999)
-                l--;
         }
-        Arrays.sort(nums);
-        return (nums.length-duplicates);
+        return indexToInsert;
     }
 
     public static void main(String[] args) {
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        //int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int[] nums = {-3,-1,0,0,0,3,3};
         int k = new RemoveDuplicates().removeDuplicates(nums);
         System.out.println("k = " + k);
         for(int i:nums)
