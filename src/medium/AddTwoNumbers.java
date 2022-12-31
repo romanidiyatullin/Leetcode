de89db2fd28ad6e2bc2277dcb8063f07bfe488bb
idiyrom <roman.idiyatullin@gmail.com>
2022-12-31 13:54:22 -0500

    Leetcode 2 (Moderate) - AddTwoNumbers

---
+++ src/medium/AddTwoNumbers.java
@@ -0,0 +1,17 @@
+package medium;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
