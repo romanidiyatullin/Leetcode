7dc268b714ba2caae7808149dee6cbc2f5ad80aa
idiyrom <roman.idiyatullin@gmail.com>
2023-01-07 19:33:47 -0500

    Leetcode 2 (MEDIUM) - Add Two Numbers. Solution Attempt 1: SUCCESS https://leetcode.com/problems/add-two-numbers/submissions/872397797/
    Runtime - beats 38.3% (within 4ms)
    Memory - beats 37.13% (47.8 MB)

--- src/medium/AddTwoNumbers.java
+++ src/medium/AddTwoNumbers.java
@@ -12,6 +12,99 @@ class ListNode {
 
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+
+        if(l1 == null || l2 == null)
+            return null;
+
+        ListNode l3 = null;
+        ListNode l3head = null;
+
+        int remnants = 0;
+
+        int sum = 0;
+
+        while(l1!=null && l2!=null)
+        {
+            sum = l1.val + l2.val + remnants;
+
+            if(sum>9) {
+                remnants = 1;
+                sum = sum % 10;
+            }
+            else {
+                remnants = 0;
+            }
+
+            if (l3head == null) {
+                l3head = new ListNode(sum, null);
+                l3 = l3head;
+            }
+            else {
+                l3.next = new ListNode(sum, null);
+                l3 = l3.next;
+            }
+
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        if(l1 == null && l2 == null) {
+            if(remnants != 0) {
+                l3.next = new ListNode(remnants, null);
+            }
+            return l3head;
+        }
+
+        if(l1 == null) {
+            if(remnants > 0) {
+                sum = l2.val + remnants;
+                if (sum > 9) addRemnant(l2);
+                else l2.val = sum;
+            }
+            l3.next = l2;
+        }
+        else {
+            if(remnants > 0) {
+                sum = l1.val + remnants;
+                if (sum > 9) addRemnant(l1);
+                else l1.val = sum;
+            }
+            l3.next = l1;
+        }
+
+        return l3head;
+    }
+
+    public void addRemnant(ListNode list){
+
+        int remnants = 1;
+        int sum = 0;
+        ListNode currentListNode = null;
+
+        while(list!=null){
+            sum = list.val + remnants;
+            if(sum > 9) {
+                list.val = sum %10;
+            }
+            else {
+                list.val = sum;
+                remnants = 0;
+                break;
+            }
+            currentListNode = list;
+            list = list.next;
+        }
+        if(remnants > 0)
+            currentListNode.next = new ListNode(remnants);
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(9, new ListNode(9));
+        ListNode l2 = new ListNode(9);
+        ListNode result = new AddTwoNumbers().addTwoNumbers(l1, l2);
+        while(result!=null){
+            System.out.println(result.val + " ");
+            result = result.next;
+        }
     }
 }
