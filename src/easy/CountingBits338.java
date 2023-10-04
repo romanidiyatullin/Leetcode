5c0f0dbfe22ef5c6ccc17d93706af04cbc689511
idiyrom <roman.idiyatullin@gmail.com>
2023-10-04 19:46:50 -0400

    https://leetcode.com/problems/counting-bits/submissions/1067252668/
    Runtime beats 99%
    Memory beats 58%

--- src/easy/CountingBits338.java
+++ src/easy/CountingBits338.java
@@ -3,18 +3,21 @@ package easy;
 import java.util.Arrays;
 
 public class CountingBits338 {
+
     public int[] countBits(int n) {
+
         if(n>100000)
             return new int[0];
+
         n++;
         int[] arr = new int[n];
         arr[0] = 0;
+
         for(int i=1; i<arr.length; i++){
             int j = i;
             int counter =0;
             while(j>0){
                 j = j &(j-1);
-                System.out.println("J=" +j);
                 counter++;
             }
             arr[i] = counter;
@@ -22,7 +25,23 @@ public class CountingBits338 {
         return arr;
     }
 
+    public int[] countBitsSmart(int n) {
+
+        if(n>100000) return new int[0];
+
+        n++;
+        int[] numberOf1InIndex = new int[n];
+        numberOf1InIndex[0] = 0;
+
+        // P(x) = P(x/2) + x(mod)2
+        for(int x = 1; x<numberOf1InIndex.length; x++){
+            numberOf1InIndex[x] = numberOf1InIndex[x/2] + x%2;
+        }
+        return numberOf1InIndex;
+    }
+
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new CountingBits338().countBits(5)));
+        System.out.println(Arrays.toString(new CountingBits338().countBitsSmart(5)));
     }
 }
