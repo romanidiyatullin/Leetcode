63b364098f310e3a8f0974b190d2812e5c465fc2
idiyrom <roman.idiyatullin@gmail.com>
2023-10-21 20:23:26 -0400

    Small performance tune: Why do I use time-costly length() if I can compute length of substring purely via indexes! https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/1080931326/
    Runtime 90%
    Memory 70%

--- src/medium/LongestSubstringNoRepeatingChars3.java
+++ src/medium/LongestSubstringNoRepeatingChars3.java
@@ -4,19 +4,18 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class LongestSubstringNoRepeatingChars3 {
-    public static int lengthOfLongestSubstring(String s) {
 
+    public static int lengthOfLongestSubstring(String s) {
+        if(s.length()==0) return 0;
         Map<Character, Integer> m = new HashMap<>();
         int start = 0;
         int max = 0;
 
         for(int i=0; i<s.length(); i++){
-            if(m.containsKey(s.charAt(i))) {
+            if(m.containsKey(s.charAt(i)))
                 start = Math.max(start, m.get(s.charAt(i))+1);
-                m.put(s.charAt(i),i);
-            }
-            m.put(s.charAt(i), i);
-            max = Math.max(max, s.substring(start,i+1).length());
+            m.put(s.charAt(i),i);
+            max = Math.max(max, (i-start)+1);
         }
         return max;
     }
