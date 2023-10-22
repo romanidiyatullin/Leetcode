5af9dc23173b6da85cc450632ab8dd74e6d3ff01
idiyrom <roman.idiyatullin@gmail.com>
2023-10-21 20:00:01 -0400

    https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/1080920251/
    Runtime 39%
    Memory 9%

---
+++ src/easy/LongestSubstringNoRepeatingChars3.java
@@ -0,0 +1,31 @@
+package easy;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class LongestSubstringNoRepeatingChars3 {
+    public static int lengthOfLongestSubstring(String s) {
+
+        Map<Character, Integer> m = new HashMap<>();
+        int start = 0;
+        int max = 0;
+
+        for(int i=0; i<s.length(); i++){
+            if(m.containsKey(s.charAt(i))) {
+                start = Math.max(start, m.get(s.charAt(i))+1);
+                m.put(s.charAt(i),i);
+            }
+            m.put(s.charAt(i), i);
+            max = Math.max(max, s.substring(start,i+1).length());
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+       System.out.println(lengthOfLongestSubstring("abcadzxvpma")); //9
+       System.out.println(lengthOfLongestSubstring("aaa"));  //1
+       System.out.println(lengthOfLongestSubstring("abcdqwertyd")); //10
+       System.out.println(lengthOfLongestSubstring("abba")); //2
+    }
+
+}
